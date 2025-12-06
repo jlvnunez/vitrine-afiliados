42d34d0a00f4956e2d83c2a17a2e9a75dfa0e7e8
jlvnunez <jlvnunez001@gmail.com>
2025-12-05 22:17:53 -0300

    update vitrine afliados

--- afiliado.js
+++ afiliado.js
@@ -3,7 +3,7 @@ const destaqueProdutos = [
   {
     id: 1,
     titulo: "Smart Tv Philco 50 P50eaa 4k Led Android Tv Dolby Audio 110v/220vmartwatch avançado",
-    preco: "",
+    //preco: "R$ 1695,63",
     rating: 4.9,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_985720-MLA99931783357_112025-F.webp",
     //badge: "NOVIDADE",
@@ -12,7 +12,7 @@ const destaqueProdutos = [
   {
     id: 2,
     titulo: "Jogo 4 Pneus Itaro Aro 14 IT203 175/75R14 86T",
-   preco: "",
+    //preco: "1.203,33",
     rating: 4.9,
     img: "https://down-bs-br.img.susercontent.com/sg-11134201-7rdx5-lxla4ft03iau53.webp",
     //badge: "TOP SELLER",
@@ -21,7 +21,7 @@ const destaqueProdutos = [
   {
     id: 3,
     titulo: "Notebook HP 256R G9 Intel Core i3-1315U 15.6 HD Intel UHD Graphics 256GB SSD 8GB RAM Windows 11 Home 64 SL - BQ9K8AT#AK4",
-    preco: "",
+    //preco: "R$ 2.249,10",
     rating: 4.4,
     img: "https://http2.mlstatic.com/D_NQ_NP_2X_774849-MLA99987332081_112025-F.webp",
     //badge: "OFERTA",
@@ -33,7 +33,7 @@ const destaqueProdutos = [
 const topSellers = [
   { id: 4, 
     titulo: "Console PlayStation 5 Slim Digital 825GB",
-     preco: "", 
+     //preco: "R$ 2.999,00", 
      rating: 5.0, 
      img:"https://down-br.img.susercontent.com/file/br-11134207-81z1k-mfmltid08v7o56.webp", 
      /*badge: "TOP",*/
@@ -41,14 +41,14 @@ const topSellers = [
 
   { id: 5, 
     titulo: "Furadeira E Parafusadeira De Impacto Sem Fio A Bateria 21v 3/8 Com Maleta E Acessorios Tb-21pzw The Black Tools",
-    preco: "",
+    // preco: "R$ 169,90",
      rating: 4.8, img: "https://http2.mlstatic.com/D_NQ_NP_2X_665097-MLA98169045976_112025-F.webp", 
      //badge: "NOVIDADE",
       link: "https://mercadolivre.com/sec/1XiRgHC" },
   
       { id: 6, 
      titulo: "Apple iPhone 16 (128 GB) - Branco - Distribuidor Autorizado",
-     preco: "", 
+     // preco: "R$ 4.648,91", 
      rating: 4.0, img: "https://http2.mlstatic.com/D_NQ_NP_2X_810764-MLA79138886319_092024-F.webp",
       //badge: "TOP", 
       link: "https://mercadolivre.com/sec/2KXLNig" }
@@ -57,27 +57,27 @@ const topSellers = [
 const gridProdutos = [
   { id: 7,
      titulo: "Kit 4 Blusas Feminina Fitness Dry Fit Camiseta Academia",
-     preco: "",
+    // preco: "R$ 57,39",
      rating: 4.7, 
      img: "https://http2.mlstatic.com/D_NQ_NP_2X_920257-MLB91454160279_092025-F-kit-4-blusas-feminina-fitness-dry-fit-camiseta-academia.webp", 
      link: "https://mercadolivre.com/sec/2PbHZKP" },
 
   { id: 8,
     titulo: "Motor de Portão Completo Dz Casa Speed 7 segundos 400kg 2 Controles 3m Cremalheira",
-    preco: "",
+   // preco: "R$ 429,88",
     rating: 4.9, img: "https://down-bs-br.img.susercontent.com/br-11134207-81z1k-mfzfwgji7rb9e2.webp", 
     link: "https://s.shopee.com.br/5fhLqiZa2K" },
 
   { id: 9, 
   titulo: "Aminomix Pet 100G" ,
-  preco: "", 
+  //preco: "R$ 38,90", 
   rating: 5.0, 
   img: "https://down-tx-br.img.susercontent.com/br-11134207-7r98o-m0lmc65ao3a816.webp", 
   link: "https://s.shopee.com.br/8fKyy6cXbo" }, 
  
   { id:10,
   titulo: "40 Dias Cheios do Espírito Santo | Kennedy Carvalho",
-   preco: "", 
+   //preco: "R$ 19,90", 
    rating: 5.0, 
    img: "https://down-tx-br.img.susercontent.com/br-11134207-7r98o-m8c60eran9wxa1.webp",
    link: "https://s.shopee.com.br/5L4X0MFCrB" },]
@@ -105,7 +105,7 @@ function criarSlide(prod) {
         <img src="${prod.img}" alt="${prod.titulo}">
         <div>
           <h3>${prod.titulo}</h3>
-         <div class="price">${prod.preco}</div> 
+         <div class="price">${prod.preco}</div>
           <div class="rating">${estrelas(prod.rating)}</div>
           <div class="actions">
             <a class="btn" href="${prod.link}" target="_blank" rel="noopener">Ver detalhes</a>
@@ -125,7 +125,7 @@ function criarSlide(prod) {
   const dots = root.querySelector(".dots");
   const prev = root.querySelector(".prev");
   const next = root.querySelector(".next");
-  const interval = Number(root.dataset.interval || 5000);
+  const interval = Number(root.dataset.interval || 3000);
   let index = 0, timer = null;
 
   // Render
